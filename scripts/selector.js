e4f03a92d79fc1cd57c37a977d3283eb68ebc022
Aski <adrianskrzypczyski@gmail.com>
2024-01-13 16:57:47 +0100

    Logo
    Co-authored-by: WarterPL <WarterPL@users.noreply.github.com>

--- scripts/selector.js
+++ scripts/selector.js
@@ -9,3 +9,12 @@ function SelectPanel(ID)
     Content.innerHTML = "<embed src=\"pages/" + PanelsKeys[ID] + ".html\">";
     Title.innerHTML = PanelsKeys[ID]
 }
+function SelectMain()
+{
+    const Content = document.getElementById("rightPanel");
+    const Title = document.getElementById("WWWTitle");
+
+    SelectedPanel = ID;
+    Content.innerHTML = "<embed src=\"pages/starter.html\">";
+    Title.innerHTML = PanelsKeys[ID]
+}
