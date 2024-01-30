a2e6305f47fcb91500d1c2cd296ba6690b806fdd
WarterPL <Warter@github.com>
2024-01-30 22:36:07 +0100

    Ask for cookies (with saving one cookie XD)
    więcej ciastek

--- scripts/cookiesHandler.js
+++ scripts/cookiesHandler.js
@@ -1,5 +1,35 @@
+let AllowedCookies = false;
+
+let cookie_consent = GetCookie("user_cookie_consent");
+if(cookie_consent == "ENABLED")
+{
+    document.getElementById("acceptCookies").style.display = "none";
+    AllowedCookies = true;
+    SetCookie('user_cookie_consent', "ENABLED", 30);
+}
+else
+{
+    document.getElementById("acceptCookies").style.display = "flex";
+    AllowedCookies = false;
+}
+
+function EnableCookies(isEnabled)
+{
+    if(isEnabled)
+    {
+        DeleteCookie('user_cookie_consent');
+        SetCookie('user_cookie_consent', "ENABLED", 30);
+        AllowedCookies = false;
+        document.getElementById("acceptCookies").style.display = "none";
+    }
+    else
+    {
+        SetCookie('user_cookie_consent', "DISABLED", 30);
+        window.history.go(-1);
+    }
+}
 function SetCookie(Name, Value, DaysToLive = 90)
-{ 
+{
     const data = new Date();
     data.setTime(data.getTime() + (DaysToLive*24*60*60*1000));
     let expires = `expires=${data.toUTCString()};`;
