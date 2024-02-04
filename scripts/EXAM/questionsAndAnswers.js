67260b366f83c65bdd87a6975367c060c48209e1
WarterPL <Warter@github.com>
2024-02-04 21:22:07 +0100

    Some Questions fixes

--- scripts/EXAM/questionsAndAnswers.js
+++ scripts/EXAM/questionsAndAnswers.js
@@ -1,30 +1,47 @@
 let selectedQuestion = 0;
 
+const ButtonPanel = document.getElementById("eButtons");
 const QuestionsPanel = document.getElementById("eQuestion");
 const AnswerPanel = document.getElementById("eAnswer");
+
 const QuestionButtons = document.querySelectorAll(".eSelector");
 
 let AssignedQuestions = { };
+let AssignedAnswers = { };
 
-for(let i = 0; i < 20; i++)
+for(let i = 0; i < 4; i++)
 {
     let pytanie = Math.floor(Math.random() * QuestionsKeys.length);
-    console.log(pytanie);
-    //QuestionsPanel.innerHTML += QuestionsKeys[pytanie];
+    ButtonPanel.innerHTML += `<input type="button" value="${i+1}" class="eSelector" onClick="selectQuestion(${i})">`;
     AssignedQuestions[i] = QuestionsKeys[pytanie];
+    console.log(pytanie);
+
     //Losowanie pytań z listy (trzeba dodać, żeby się nie powtarzały ale najpierw potrzebujemy min 20 pytań
     //w generate questions)
     // schemat pytań {"pytanie":[index poprawnej odpowiedzi, "A", "B", "C", "D"]}
 }
 selectQuestion(0);
-
+console.log(AssignedQuestions);
 
 function selectQuestion(id)
 {
-    AnswerPanel.innerHTML = "";
-    QuestionsPanel.innerHTML = AssignedQuestions[id];
+    selectedQuestion = Number(id);
+    QuestionsPanel.innerHTML = AssignedQuestions[Number(id)];
+    const answ1 = document.getElementById("answ1");
+    const answ2 = document.getElementById("answ2");
+    const answ3 = document.getElementById("answ3");
+    const answ4 = document.getElementById("answ4");
+    answ1.innerHTML = Questions[AssignedQuestions[Number(id)]][1];
+    answ2.innerHTML = Questions[AssignedQuestions[Number(id)]][2];
+    answ3.innerHTML = Questions[AssignedQuestions[Number(id)]][3];
+    answ4.innerHTML = Questions[AssignedQuestions[Number(id)]][4];
+    /*AnswerPanel.innerHTML = "";
     for(let i = 1; i <= 3; i++)
     {
         AnswerPanel.innerHTML += Questions[QuestionsKeys[id]][i];
-    }
+    }*/
+}
+function selectAnswer(id)
+{
+    AssignedAnswers[selectedQuestion] = id;
 }
